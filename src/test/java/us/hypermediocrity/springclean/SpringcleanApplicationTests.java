5231ae5919e5dc324e81914455fc5829417c7b8e
mjeffw <geek.macbeer@gmail.com>
2022-02-22 09:23:35 -0500

    initial commit

---
+++ src/test/java/us/hypermediocrity/springclean/SpringcleanApplicationTests.java
@@ -0,0 +1,13 @@
+package us.hypermediocrity.springclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
