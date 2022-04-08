76d9b88cfb6f007e6340dbbb3b68a0e70b6ece26
mjeffw <geek.macbeer@gmail.com>
2022-04-08 09:10:06 -0400

    wip commit

---
+++ application/src/test/java/us/hypermediocrity/springclean/SpringCleanArchitectureTest.java
@@ -0,0 +1,15 @@
+package us.hypermediocrity.springclean;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+@AnalyzeClasses(packages = "us.hypermediocrity.springclean")
+class SpringCleanArchitectureTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
